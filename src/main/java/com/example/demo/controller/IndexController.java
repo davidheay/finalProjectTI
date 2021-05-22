cd1b669725617272a8c8d2993b70c5d84d75f3f6
davidheay <deha121212@outlook.es>
2021-05-22 18:39:41 -0500

    Final change

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,20 +1,51 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Imagen;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @RequestMapping("")
 public class IndexController {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @GetMapping
-    public String index() {
-        return jdbcTemplate.queryForObject("select id from imgs", String.class);
+    @GetMapping("")
+    public String index(Model model) {
+        model.addAttribute("imgs", jdbcTemplate.query("select * from imgs;", (rs, i) -> {
+            return new Imagen(rs.getInt("id"), rs.getString("img"));
+        }));
+        return "index";
+    }
+
+    @PostMapping("/insert")
+    @ResponseBody
+    public Boolean insert(@RequestBody String img) {
+        try {
+            return jdbcTemplate.update("insert into imgs (img) values (?);", new Object[] { img }) >= 1;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    @GetMapping("/delete/{id}")
+    @ResponseBody
+    public Boolean delete(@PathVariable Integer id) {
+        try {
+            return jdbcTemplate.update("delete from imgs where id=?;", new Object[] { id }) >= 1;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
