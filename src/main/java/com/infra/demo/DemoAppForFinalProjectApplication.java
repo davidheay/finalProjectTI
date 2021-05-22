a00b0838d5ebfca82fbb6018eff119d16f1239c1
davidheay <deha121212@outlook.es>
2021-05-22 10:04:27 -0500

    spring app

---
+++ src/main/java/com/infra/demo/DemoAppForFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.infra.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppForFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppForFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infra/demo/DemoAppForFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infra.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppForFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
