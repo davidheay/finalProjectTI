fdd613dbc6c44f23e28fbf5f466b7aeefe69adb4
David Herrera Ayala <44204891+davidheay@users.noreply.github.com>
2021-05-22 18:43:13 -0500

    Test change

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -15,6 +15,6 @@ public class IndexController {
 
     @GetMapping
     public String index() {
-        return jdbcTemplate.queryForObject("select id from imgs", String.class);
+        return jdbcTemplate.queryForObject("select id from imgs", String.class) + "hola";
     }
 }
